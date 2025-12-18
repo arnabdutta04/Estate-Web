199201f10ece2404ee61af7aedc56cdc1d106ea5
arnabdutta04 <arnabdutta453@gmail.com>
2025-12-18 23:45:04 +0530

    Debug MongoDB env variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.log("MONGODB_URI =", process.env.MONGODB_URI);
+
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     console.log('MongoDB Connected Successfully');
   } catch (error) {
     console.error('MongoDB Connection Error:', error.message);
