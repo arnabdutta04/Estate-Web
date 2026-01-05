336162d7726786152ab7331b1fed86fdc660860c
arnabdutta04 <arnabdutta453@gmail.com>
2026-01-05 11:01:51 +0530

    Fix Navbar filename case for Linux build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 import React from "react";
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "./context/ThemeContext";
 
@@ -16,52 +16,60 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import "./App.css";
 
 function AppContent() {
+  const location = useLocation();
+  const isAuthPage = location.pathname === "/login" || location.pathname === "/register";
+
   return (
-    <Routes>
-      {/* WELCOME = HOME */}
-      <Route path="/" element={<Welcome />} />
-      <Route path="/home" element={<Navigate to="/" replace />} />
+    <>
+      {/* Render Welcome page in background when on auth pages */}
+      {isAuthPage && <Welcome />}
       
-      <Route
-        path="/properties"
-        element={
-          <ProtectedRoute>
-            <Properties />
-          </ProtectedRoute>
-        }
-      />
+      <Routes>
+        {/* WELCOME = HOME */}
+        <Route path="/" element={<Welcome />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        
+        <Route
+          path="/properties"
+          element={
+            <ProtectedRoute>
+              <Properties />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/properties/:id"
-        element={
-          <ProtectedRoute>
-            <PropertyDetail />
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/properties/:id"
+          element={
+            <ProtectedRoute>
+              <PropertyDetail />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/brokers"
-        element={
-          <ProtectedRoute>
-            <Brokers />
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/brokers"
+          element={
+            <ProtectedRoute>
+              <Brokers />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/profile"
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
 
-      {/* AUTH */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+        {/* AUTH - Render as modals over Welcome page */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
