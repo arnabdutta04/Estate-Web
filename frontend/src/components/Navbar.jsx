df4ac49c2f4870c266ee150687c2696c4e7791de
arnabdutta04 <arnabdutta453@gmail.com>
2026-01-08 00:52:37 +0530

    Fix FaTrendingUp import from fa6 correctly

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import "/navbar.css";
 import {
   FaUser,
   FaSignOutAlt,
