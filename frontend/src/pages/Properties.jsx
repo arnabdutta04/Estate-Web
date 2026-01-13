2a4073c8e59ae0d77933ba828d08e12896e1b821
arnabdutta04 <arnabdutta453@gmail.com>
2026-01-13 10:48:48 +0530

    Fix App.css import path

--- frontend/src/pages/Properties.jsx
+++ frontend/src/pages/Properties.jsx
@@ -6,6 +6,7 @@ import Navbar from "../components/Navbar";
 import PageTransition from '../components/PageTransition';
 import { FaChevronDown, FaHome } from 'react-icons/fa';
 import './Properties.css';
+import img from "/property.jpg";
 
 const Properties = () => {
   const navigate = useNavigate();
